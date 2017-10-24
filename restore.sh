f2cdb7dac12370c3a41559bedcb8d50a5520d438
devishian <33056613+devishian@users.noreply.github.com>
2017-10-24 13:38:31 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,1 +1,1 @@
- docker run --rm --volumes-from managerio_data_1 -v $(pwd):/backup devishian/manager.io bash -c "cd / && tar xzvf /backup/Manager-backup.tar.gz"
+ docker run --rm --volumes-from managerio_data_1 -v $(pwd):/backup mprokopov/manager.io bash -c "cd / && tar xzvf /backup/Manager-backup.tar.gz"
